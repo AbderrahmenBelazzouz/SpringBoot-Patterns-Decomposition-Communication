5d3d1e64c0d6a2c8d472bdffc02f07eca6788e9b
Abderrahmen Belazzouz <a.belazzouz@esi-sba.dz>
2021-11-29 23:16:40 +0100

    Add files via upload

---
+++ ms-formation/src/main/java/com/esi/msformation/entities/Formation.java
@@ -0,0 +1,23 @@
+package com.esi.msformation.entities;
+
+import com.esi.msformation.model.Etudiant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Formation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFormation;
+    private String nom;
+    private int duree;
+
+    @Transient
+    private Collection<Etudiant> etudiants;
+}
